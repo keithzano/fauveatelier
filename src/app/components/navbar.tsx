4fd16361b8f5e92399b70baebdd4f315b0dd4147
keithzano <keithzano@gmail.com>
2025-04-20 13:29:21 +0200

    add navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,3 +1,93 @@
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+
+const navigation = [
+  { name: "Home", href: "#", current: true },
+  { name: "Services", href: "#", current: false },
+  { name: "Meet the Team", href: "#", current: false },
+  { name: "Get in Touch", href: "#", current: false },
+];
+
+function classNames(...classes: (string | boolean | undefined)[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
 export const Navbar = () => {
-  return <section></section>;
+  return (
+    <Disclosure as="nav" className="bg-teal-mine sticky top-0 z-20">
+      <div className="mx-auto px-2.5 sm:px-4 md:px-6 lg:px-10">
+        <div className="flex h-16 items-center justify-between">
+          <div className="flex shrink-0 items-center">
+            <img
+              alt="Fauve Atelier"
+              src="images/logo1.jpg"
+              className="h-12 w-auto"
+            />
+          </div>
+
+          <div className="flex items-center sm:hidden">
+            {/* Mobile menu button*/}
+            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset">
+              <span className="absolute -inset-0.5" />
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon
+                aria-hidden="true"
+                className="block size-6 group-data-open:hidden"
+              />
+              <XMarkIcon
+                aria-hidden="true"
+                className="hidden size-6 group-data-open:block"
+              />
+            </DisclosureButton>
+          </div>
+
+          <div className="hidden sm:ml-6 sm:block">
+            <div className="flex space-x-4">
+              {navigation.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  aria-current={item.current ? "page" : undefined}
+                  className={classNames(
+                    item.current
+                      ? "bg-teal-mine-dark text-white"
+                      : "hover:bg-teal-mine-light text-gray-300 hover:text-white",
+                    "rounded-md px-3 py-2 text-sm font-medium",
+                  )}
+                >
+                  {item.name}
+                </a>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile menu items*/}
+      <DisclosurePanel className="sm:hidden">
+        <div className="space-y-1 px-2 pt-2 pb-3">
+          {navigation.map((item) => (
+            <DisclosureButton
+              key={item.name}
+              as="a"
+              href={item.href}
+              aria-current={item.current ? "page" : undefined}
+              className={classNames(
+                item.current
+                  ? "bg-teal-mine-dark text-white"
+                  : "hover:bg-teal-mine-light text-gray-300 hover:text-white",
+                "block rounded-md px-3 py-2 text-base font-medium",
+              )}
+            >
+              {item.name}
+            </DisclosureButton>
+          ))}
+        </div>
+      </DisclosurePanel>
+    </Disclosure>
+  );
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Manrope, Playfair_Display } from "next/font/google";
 import "./globals.css";
+import { Navbar } from "./components/navbar";
 
 const manrope = Manrope({
   subsets: ["latin"],
@@ -31,6 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${playfair.variable} ${manrope.variable} antialiased`}>
+        <Navbar />
         {children}
       </body>
     </html>
